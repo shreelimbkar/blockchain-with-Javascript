b41d346862da6c1e9ae632dfdf62b49deba4dd9b
shreelimbkar <limbkarshree@gmail.com>
2018-04-02 17:08:14 +0530

    added Mining Block method
    added Mining Block method

--- main.js
+++ main.js
@@ -7,16 +7,27 @@ class Block {
         this.data = data;
         this.previoushash = previoushash;
         this.hash = '';
+        this.nonce = 0;
     }
 
     calculateHash() {
-        return SHA256(this.index + this.previoushash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previoushash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log('Blocked mined: ', this.hash);
     }
 }
 
 class Blockchain {
     constructor() {
         this.chain = [this.createShreesBlock()];
+        this.difficulty = 4;
     }
 
     createShreesBlock() {
@@ -29,7 +40,8 @@ class Blockchain {
 
     addBlock(newBlock) {
         newBlock.previoushash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        // newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -51,9 +63,12 @@ class Blockchain {
  }
 
 let shreeCoin = new Blockchain();
+console.log('Mining Block 1...');
 shreeCoin.addBlock(new Block(1, '02/04/2018', { amount : 4}));
+
+console.log('Mining Block 2...');
 shreeCoin.addBlock(new Block(2, '01/04/2018', { amount: 10 }));
 
 
-console.log(JSON.stringify(shreeCoin, null, 4));
-console.log('Is blockchain is Valid? ', shreeCoin.isChainValid());
+// console.log(JSON.stringify(shreeCoin, null, 4));
+// console.log('Is blockchain is Valid? ', shreeCoin.isChainValid());
